793776d12c9264d601c8394a07821888d8ba1702
CiroAntony <vasquezciro654@gmail.com>
2024-03-02 12:17:37 -0500

    loading cocktail page

---
+++ js/cocteles.js
@@ -0,0 +1,57 @@
+const urlParams = new URLSearchParams(window.location.search);
+const cocktailId = urlParams.get("id");
+
+const apiUrl =
+  `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`;
+
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("La url no responde");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    const container = document.querySelector(".container");
+    const cocktailDetails = document.createElement("div");
+    cocktailDetails.classList.add("cocktail-details");
+
+    const ctnImage = document.createElement("img");
+    ctnImage.classList.add("image-cocktail");
+    ctnImage.src = data.drinks[0].strDrinkThumb;
+    cocktailDetails.appendChild(ctnImage);
+
+    const name = document.createElement("h2");
+    name.textContent = data.drinks[0].strDrink;
+    cocktailDetails.appendChild(name);
+
+    const ingredientsList = document.createElement("ul");
+    const ingredients = [
+      data.drinks[0].strIngredient1,
+      data.drinks[0].strIngredient2,
+      data.drinks[0].strIngredient3,
+      data.drinks[0].strIngredient4,
+      data.drinks[0].strIngredient5,
+      data.drinks[0].strIngredient6,
+      data.drinks[0].strIngredient7,
+    ];
+    const ingredientItems = ingredients.map((ingredient) => {
+      if (ingredient) {
+        const li = document.createElement("li");
+        li.textContent = ingredient;
+        return li.outerHTML;
+      } else {
+        return "";
+      }
+    });
+
+    ingredientsList.innerHTML = ingredientItems.join("");
+    cocktailDetails.appendChild(ingredientsList);
+
+    container.appendChild(cocktailDetails);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
---
+++ js/main.js
@@ -0,0 +1,39 @@
+const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const drinks = () => {
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("La url no responde");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      data.drinks.forEach((drink) => {
+        const container = document.querySelector(".container");
+        const cocktails = document.createElement("div");
+        cocktails.classList.add("drinks-container");
+        const ctnImage = document.createElement("img");
+        ctnImage.src = drink.strDrinkThumb;
+        cocktails.appendChild(ctnImage);
+
+        const link = document.createElement("a");
+        link.classList.add("links");
+        link.href = `cocteles.html?id=${drink.idDrink}`;
+
+        const ctnName = document.createElement("li");
+        ctnName.classList.add("name");
+        ctnName.textContent = drink.strDrink;
+        cocktails.appendChild(ctnName);
+        container.appendChild(link);
+        link.appendChild(cocktails);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+drinks();
