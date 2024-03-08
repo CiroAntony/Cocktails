520355dcad52efe6b8f27fbf46858dbdf3b90315
CiroAntony <vasquezciro654@gmail.com>
2024-03-08 15:50:20 -0500

    seccion de comentarios agregado

--- js/main.js
+++ js/main.js
@@ -12,19 +12,26 @@ fetch(urlApi)
     const drinksToShow = data.drinks.slice(0,10)
 
     drinksToShow.forEach((drink) => {
-        const ctndrinks = document.querySelector(".drinks-top");
+        const ctndrinks = document.querySelector(".cards-container");
+        const link = document.createElement("a");
+        link.classList.add("links");
+        link.href = `info-cocktail.html?id=${drink.idDrink}`;
+
         const  cardDrinks = document.createElement('div');
         cardDrinks.classList.add("card")
 
         const  imgDrg = document.createElement('img')
+        imgDrg.classList.add("top-img");
         imgDrg.src = drink.strDrinkThumb;
         
         const title = document.createElement('p');
+        title.classList.add("top-name")
         title.textContent = drink.strDrink;
 
         cardDrinks.appendChild(imgDrg)
         cardDrinks.appendChild(title)
-        ctndrinks.appendChild(cardDrinks);
+        link.appendChild(cardDrinks);
+        ctndrinks.appendChild(link);
     })
 })
 .catch((error) => console.log('Error: ' + error));
